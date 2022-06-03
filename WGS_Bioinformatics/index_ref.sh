50f80b91db264208d926bf68bb2a9aa74d3ff5eb
KathleenMcLay <81064774+KathleenMcLay@users.noreply.github.com>
2022-06-03 16:27:28 +1000

    Update and rename index_ref.sh to 2. index reference genome

--- WGS_Bioinformatics/index_ref.sh
+++
@@ -1,7 +0,0 @@
-#STEP 2: INDEX REFERENCE GENOME - BWA
-#does not need to be run in parallel
-
-module load bwa
-
-#set the file path  
-bwa index -a is /scratch/ht96/km6006/Ref_Genome/SLv141Asm_Ch20RN.fasta
