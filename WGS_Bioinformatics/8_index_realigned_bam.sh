74288d6792af49fd211391862e6bae12f4f6d559
KathleenMcLay <81064774+KathleenMcLay@users.noreply.github.com>
2022-06-21 12:38:23 +1000

    Delete 8_index_realigned_bam.sh

--- WGS_Bioinformatics/8_index_realigned_bam.sh
+++
@@ -1,9 +0,0 @@
-#STEP 8: INDEX REALIGNED BAMS 
-
-module load samtools
-
-#create directory variable
-INPUTDIR="/g/data/ht96/McLay_UQ/Honours/Output/WGS/realign_indels"
-
-#index the .bam file
-samtools index ${INPUTDIR}/${1}_realigned.PCRmrkd.clean.sorted.bam
