29a082df2fef7ded7b611f47c2ebeaff685be800
KathleenMcLay <81064774+KathleenMcLay@users.noreply.github.com>
2022-06-21 12:38:06 +1000

    Delete 7.2_index_reference_genome.sh

--- WGS_Bioinformatics/7.2_index_reference_genome.sh
+++
@@ -1,9 +0,0 @@
-#STEP 7: REALIGNING AROUND INDELS
-
-#7.2: INDEX THE REFERENCE GENOME
-#does not need to be run in parallel 
-
-module load samtools
-
-#indexing the reference genome with samtools for use with GATK
-samtools faidx /scratch/ht96/km6006/Ref_Genome/SLv141Asm_Ch20RN.fasta
