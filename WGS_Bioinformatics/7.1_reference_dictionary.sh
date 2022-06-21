9f5e7e6e392e66750ac40b05fe4074b73ceb616c
KathleenMcLay <81064774+KathleenMcLay@users.noreply.github.com>
2022-06-21 12:37:58 +1000

    Delete 7.1_reference_dictionary.sh

--- WGS_Bioinformatics/7.1_reference_dictionary.sh
+++
@@ -1,7 +0,0 @@
-#STEP 7: REALIGNING AROUND INDELS
-
-#7.1: MAKE A DICTIONARY FILE OF THE REFERENCE GENOME
-#does not need to be run in parallel 
-
-#creating a dictionary file of the reference genome for GATK to use in indel realignment steps
-java -jar /home/564/km6006/picard.jar CreateSequenceDictionary R=/scratch/ht96/km6006/Ref_Genome/SLv141Asm_Ch20RN.fasta O=/scratch/ht96/km6006/Ref_Genome/SLv141Asm_Ch20RN.dict
