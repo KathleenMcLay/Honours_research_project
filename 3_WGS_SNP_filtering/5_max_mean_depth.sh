3d05c6fe1bc2413f22fd51a147ef26e4d7d5c1b5
KathleenMcLay <mclay.kathleen@gmail.com>
2022-07-07 16:50:08 +1000

    .

--- 3_WGS_SNP_filtering/5_max_mean_depth.sh
+++ 3_WGS_SNP_filtering/5_max_mean_depth.sh
@@ -2,7 +2,7 @@
 DIR="/g/data/ht96/McLay_UQ/Honours/Output/WGS/SNP_filtering"
 
 #using vcftools, create a txt file, to visualise the distribution of mean depth per site that can be used to determine the mean depth filter
-/home/564/km6006/bin/vcftools --gzvcf ${DIR}/3_sites_missing_data_50.vcf.gz --site-mean-depth --out ${DIR}/mean_depth
+/home/564/km6006/bin/vcftools --gzvcf ${DIR}/3_sites_missing_data_30.vcf.gz --site-mean-depth --out ${DIR}/mean_depth
 
 #using vcftools filter the zipped VCF file to remove any sites with a mean depth >60. 
-/home/564/km6006/bin/vcftools --gzvcf ${DIR}/3_read_depth.vcf.gz --recode --recode-INFO-all max-meanDP 60 --out ${DIR}/4_max_mean_depth
+/home/564/km6006/bin/vcftools --gzvcf ${DIR}/3_sites_missing_data_30.vcf.gz --recode --recode-INFO-all max-meanDP 60 --out ${DIR}/4_30_max_mean_depth
