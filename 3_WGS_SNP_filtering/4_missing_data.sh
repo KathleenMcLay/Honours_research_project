e9a5b81f155bb10ece143b3299bde6b39297107f
KathleenMcLay <mclay.kathleen@gmail.com>
2022-07-07 20:23:49 +1000

    .

--- 3_WGS_SNP_filtering/4_missing_data.sh
+++ 3_WGS_SNP_filtering/4_missing_data.sh
@@ -2,4 +2,4 @@
 DIR="/g/data/ht96/McLay_UQ/Honours/Output/WGS/SNP_filtering"
 
 #using vcf tools, filter the zipped VCF file to remove sites where <50% of individual samples have been genotyped | save the output as a zipped VCF file
-/home/564/km6006/bin/vcftools --gzvcf ${DIR}/2_read_depth_10.vcf.gz --max-missing 0.5 --recode --recode-INFO-all --stdout | gzip -c > ${DIR}/3_sites_missing_data_50.vcf.gz
+/home/564/km6006/bin/vcftools --gzvcf ${DIR}/2_read_depth_10.vcf.gz --max-missing 0.3 --recode --recode-INFO-all --stdout | gzip -c > ${DIR}/3_sites_missing_data_30.vcf.gz
