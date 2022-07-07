068bcc696dfb950a2fb9073f547abc4e46b9e4c4
KathleenMcLay <mclay.kathleen@gmail.com>
2022-07-08 07:36:44 +1000

    .

--- 3_WGS_SNP_filtering/6_min_mean_depth.sh
+++ 3_WGS_SNP_filtering/6_min_mean_depth.sh
@@ -2,4 +2,4 @@
 DIR="/g/data/ht96/McLay_UQ/Honours/Output/WGS/SNP_filtering"
 
 #using vcftools, filter the vcf file to remove any sites with a minimum read depth <10
-/home/564/km6006/bin/vcftools --vcf ${DIR}/4_max_mean_depth.vcf min-meanDP 10 --out ${DIR}/5_min_mean_depth
+/home/564/km6006/bin/vcftools --gzvcf ${DIR}/4_30_max_mean_depth_60.vcf.gz --recode --recode-INFO-all --min-meanDP 10 --stdout | gzip -c > ${DIR}/5_30_min_mean_depth_10.vcf.gz
--- 3_WGS_SNP_filtering/7_mapping_quality.sh
+++ 3_WGS_SNP_filtering/7_mapping_quality.sh
@@ -1,2 +1,3 @@
 #set a variable for your directory 
 DIR="/g/data/ht96/McLay_UQ/Honours/Output/WGS/SNP_filtering"
+
